6235555f33f358c6b92b38985fb891b57e7a8f52
Marcelo Sampaio <marcelo@artlogica.com.br>
2017-10-03 07:16:21 -0300

    Initial Commit

---
+++ TableViewCellExpand/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewCellExpand
+//
+//  Created by Marcelo on 10/3/17.
+//  Copyright © 2017 MAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
